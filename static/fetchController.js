30bc88b896b23f6b07edb2febef3d19be5cfd673
Andre <andreas79001@gmail.com>
2023-09-17 19:39:16 +0200

    init

--- static/fetchController.js
+++ static/fetchController.js
@@ -1,59 +1,78 @@
-import Temperature from "./models/Temperature"
-$('.forecastbtn').click(async function() {
-    var thebuttonclicked =$(this).attr("value");
-   // var response = await getForecast(`${thebuttonclicked}`);
-   // console.log(response);
-   console.log("Haj");
+import { WeatherData } from "./models/WeatherData.js";
 
-    getMinTemp(`${thebuttonclicked}`);
+$('.forecastbtn').click(async function () {
+  var thebuttonclicked = $(this).attr("value");
+  var response = await getForecast(`${thebuttonclicked}`);
+  console.log(response);
+  // console.log("Haj");
 
-   //var minTemp = await getMinTemp(`${thebuttonclicked}`)
-   //console.log("Min: " + minTemp)
-   //var maxTemp = await getMaxTemp(`${thebuttonclicked}`)
-   //console.log("Max: " + maxTemp)
-    // console.log(getForecast(`${thebuttonclicked}`));
-   /* $.ajax({
-      url: `http://localhost:8080/forecast/${thebuttonclicked}`,
-      method: "GET",
-      dataType: "json", // Expected response data type
-      success: function(data) {
-        //NEXT 24 HOURS IN x CITY
-        console.log("Response Data:", data);
+  // getMinTemp(`${thebuttonclicked}`);
+  convertToObject(response)
 
-        $("#nextForecast").text("Next forecast in " + thebuttonclicked);
-        $("#nextForecastFrom").text(data[0].type + ": from " + data[0].from + data[0].unit + " to " + data[0].to + data[0].unit);
+  //var minTemp = await getMinTemp(`${thebuttonclicked}`)
+  //console.log("Min: " + minTemp)
+  //var maxTemp = await getMaxTemp(`${thebuttonclicked}`)
+  //console.log("Max: " + maxTemp)
+  // console.log(getForecast(`${thebuttonclicked}`));
+  /* $.ajax({
+     url: `http://localhost:8080/forecast/${thebuttonclicked}`,
+     method: "GET",
+     dataType: "json", // Expected response data type
+     success: function(data) {
+       //NEXT 24 HOURS IN x CITY
+       console.log("Response Data:", data);
 
-        console.log(data[0].type);
-        console.log(data[0].from);
-        console.log(data[0].to);
-        console.log(data[0].unit);
-        console.log(data[0].time);
-      },
-      error: function(xhr, status, error) {
-        console.error("AJAX Error:", status, error);
-      }
-    });*/
+       $("#nextForecast").text("Next forecast in " + thebuttonclicked);
+       $("#nextForecastFrom").text(data[0].type + ": from " + data[0].from + data[0].unit + " to " + data[0].to + data[0].unit);
+
+       console.log(data[0].type);
+       console.log(data[0].from);
+       console.log(data[0].to);
+       console.log(data[0].unit);
+       console.log(data[0].time);
+     },
+     error: function(xhr, status, error) {
+       console.error("AJAX Error:", status, error);
+     }
+   });*/
 });
 
 async function getForecast(city) {
-    let url = `http://localhost:8080/forecast/${city}`;
-    try {
-      let response = await fetch(url);
-      return await response.json();
-    } catch (error) {
-      console.log(error);
-    }
+  let url = `http://localhost:8080/forecast/${city}`;
+  try {
+    let response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
   }
+}
 
- async function mapForecast(city){
-    const response = await getForecast(city)
-    console.log("response: ", response)
-    // response.map((item) => {
-    //   switch(type)
-    //   if(item.type === "wind speed")
-    // }
-    
- }
+async function convertToObject(response) {
+  let weatherData = WeatherData(response.time, response.type, response.value, response.unit, response.place);
+  console.log(weatherData)
+
+  response.map((item) => {
+    switch (item.type) {
+      case "temperature": {
+
+        console.log("Temperature: " + item.from + item.unit + " to " + item.to + item.unit);
+        break;
+      }
+      case "precipitation": {
+
+      }
+      case "wind speed": {
+
+      }
+      case "cloud coverage": {
+
+      }
+      default: {
+        break;
+      }
+    }
+  });
+}
 
 
 
@@ -63,7 +82,7 @@ async function getMaxTemp(city) {
   let max = -99;
   for (x in weatherData) {
     let to = weatherData[x].to;
-    if(to > max) {
+    if (to > max) {
       max = to;
     }
   }
